e8effee289988b11eda3bcf6f2b0fcd5257f89eb
AnushAdk04 <adhikarianush64@gmail.com>
2026-01-18 09:40:27 +0545

    about page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,19 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-
-          {/* Placeholder routes for now */}
-          <Route path="about" element={<div className="p-10">About Page (Coming Soon)</div>} />
+          <Route path="about" element={<About />} />
+          <Route path="interests" element={<div className='p-10'>Interests Page (Coming Soon)</div>} />
           <Route path="services" element={<div className="p-10">Services Page (Coming Soon)</div>} />
-          <Route path="interests" element={<div className="p-10">Interests Page (Coming Soon)</div>} />
           <Route path="contact" element={<div className="p-10">Contact Page (Coming Soon)</div>} />
         </Route>
-
       </Routes>
     </BrowserRouter>
   );
