198bbae48eb5ec5634153d71e97945bacae175a0
AnushAdk04 <adhikarianush64@gmail.com>
2026-01-28 11:06:58 +0545

    environemnt variable added for API hiding

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
     const onSubmit = async (event) => {
         event.preventDefault();
         const formData = new FormData(event.target);
-        formData.append("access_key", "YOUR_API_KEY");
+        formData.append("access_key", import.meta.env.VITE_WEB3FORMS_API_KEY);
 
         const response = await fetch("https://api.web3forms.com/submit", {
             method: "POST",
